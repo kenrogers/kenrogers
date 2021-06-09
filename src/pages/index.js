ce37cf5da237a177e71df75f667bdeb87c850b3b
Ken Rogers <ken@kenrogers.co>
2021-06-09 17:11:44 -0600

    Change text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,69 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Ken Rogers" />
-    <h2 className="text-5xl md:text-7xl">Hi 👋🏻 my name is Ken.</h2>
-    <h1 className="text-3xl md:text-5xl font-black mt-8">
-      I use <span className="text-purple-500">words</span> and{" "}
-      <span className="text-red-500">code</span> to help businesses decrease
-      costly technical overhead and increase high-quality customers.
+    <p className="text-5xl md:text-7xl">Hi 👋🏻 I'm Ken.</p>
+    <h1 className="text-4xl md:text-6xl font-black mt-8">
+      I'm a full-stack web and blockchain developer.
     </h1>
-    <h3 className="text-2xl md:text-3xl mt-8">I can help in two ways:</h3>
-    <p className="text-xl md:text-2xl mt-8">
-      Write custom <span className="text-purple-500">email marketing</span>{" "}
-      campaigns for small businesses looking to convert more website visitors to
-      customers.
-    </p>
-    <p className="text-xl md:text-2xl mt-8">
-      Create <span className="text-red-500">custom software</span> for
-      businesses looking to streamline complex processes.
-    </p>
-    <p className="text-xl md:text-2xl mt-8">Reach out at ken@kenrogers.co</p>
+    <h2 className="text-2xl md:text-4xl mt-8">
+      I help high-caliber companies build the modern web with React and
+      Solidity.
+    </h2>
+    <h3 className="text-2xl md:text-3xl mt-8 font-bold">Courses</h3>
+    <ul>
+      <li>
+        <a
+          href="https://launchyourdevcareer.com/pro"
+          className="text-red-500 text-2xl"
+          target="_blank"
+        >
+          Launch Your Dev Career
+        </a>
+      </li>
+    </ul>
+
+    <h3 className="text-2xl md:text-3xl mt-8 font-bold">Writing</h3>
+    <ul>
+      <li>
+        <a
+          href="https://www.freecodecamp.org/news/author/ken-rogers/"
+          className="text-red-500 text-2xl"
+          target="_blank"
+        >
+          Career Growth Guides for Aspiring Developers
+        </a>
+      </li>
+    </ul>
+    <h3 className="text-2xl md:text-3xl mt-8 font-bold">Get In Touch</h3>
+    <ul>
+      <li>
+        <a
+          href="mailto:ken@kenrogers.co"
+          className="text-red-500 text-2xl"
+          target="_blank"
+        >
+          Email
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/KenTheRogers"
+          className="text-red-500 text-2xl"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://linkedin.com/in/kennetharogers"
+          className="text-red-500 text-2xl"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+      </li>
+    </ul>
   </Layout>
 )
 
